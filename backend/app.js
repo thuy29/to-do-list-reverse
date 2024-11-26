4572ff9c9b6900f0ef6d97237b4a8569da360589
Co <thuylinh.co@efrei.net>
2024-11-26 21:33:38 +0100

    Add comment in feature branch

--- backend/app.js
+++ backend/app.js
@@ -18,5 +18,5 @@ app.use('/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`); //Comment 1
+    console.log(`Server is running on port ${PORT}`); //New comment
 });
