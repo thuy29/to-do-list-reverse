0109bf0c59c89ca29c65661ead60038768e86c21
Co <thuylinh.co@efrei.net>
2024-11-26 17:16:41 +0100

    Scenario test dev to main

--- backend/app.js
+++ backend/app.js
@@ -18,5 +18,5 @@ app.use('/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`); //Comment
 });
